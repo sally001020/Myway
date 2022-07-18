df11c278cd25e09b53c5a69756516d96e1fcbbcf
2005899 <sally001020@naver.com>
2022-07-18 14:08:36 +0900

    Dialogflow_jiyeon

--- app/src/main/java/com/example/myway/interfaces/BotReply.java
+++
@@ -1,7 +0,0 @@
-package com.example.myway.interfaces;
-
-import com.google.cloud.dialogflow.v2.DetectIntentResponse;
-
-public interface BotReply {
-    void callback(DetectIntentResponse returnResponse);
-}
